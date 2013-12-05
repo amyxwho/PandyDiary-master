f8a2e982890b62b9fb2f066689797adade2b714c
amyxwho <ahu2@wellesley.edu>
2013-12-05 13:56:42 -0500

    Charts
    There are currently charts but they don't seem to be doing what they had
    done before

---
+++ PandyDiary/PandyFood.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class PandyFood : Form
+    {
+        public PandyFood()
+        {
+            InitializeComponent();
+        }
+
+        public void readData(List<int> data)
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                foodChart.Series["Series1"].Points.AddXY(i, data.ElementAt(i));
+            }
+        }
+    }
+}
---
+++ PandyDiary/PandyRest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class PandyRest : Form
+    {
+        public PandyRest()
+        {
+            InitializeComponent();
+        }
+
+        public void readData(List<int> data)
+        {
+            for (int i = 0; i < data.Count; i++)
+            {
+                restChart.Series["Series1"].Points.AddXY(i, data.ElementAt(i));
+            }
+        }
+
+    }
+}
